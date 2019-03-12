1bb88833696e378f64888336b7c125a2160f119c
divaibhav <divaibhav@gmail.com>
2019-03-12 22:00:34 +0530

    created mylinkedlist class.
    implemented void insert() to insert a node at the end of linked list

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -6,6 +6,21 @@ class Node{
 
 }
 class MySinglyLinkedList{
+    Node head;
+
+    //insert at last
+    void insert(Node node){
+        if(head==null){
+            head=node;
+        }
+        else{
+            Node temp=head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=node;
+        }
+    }
 
 }
 public class LinkedListMain {
