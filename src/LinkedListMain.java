a3f7cabbf6c9c264bb55240326b8583ad900bce1
divaibhav <divaibhav@gmail.com>
2019-03-12 22:22:21 +0530

    implemented LinkedListMain class and implemented main method. Called functions to insert new node by all insert method

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -90,4 +90,34 @@ class MySinglyLinkedList{
 }
 public class LinkedListMain {
 
+    public static void main(String[] args) {
+        MySinglyLinkedList list=new MySinglyLinkedList();
+
+        Node n1=new Node();
+        n1.data=100;
+
+        Node n2=new Node();
+        n2.data=200;
+
+        Node n3=new Node();
+        n3.data=300;
+
+        Node n4=new Node();
+        n4.data=400;
+
+        Node n5=new Node();
+        n5.data=500;
+
+        list.insert(n1);
+        list.display();
+        list.insertAtBeginning(n2);
+        list.display();
+        //inserting n3 that is 300 after n1 that is 100
+        list.insertAfterNode(n3,n1);
+        list.display();
+        //inserting n4 that is 400 beofre n2 tthat is 200
+        list.insertBeforeNode(n4,n2);
+        list.display();
+    }
+
 }
